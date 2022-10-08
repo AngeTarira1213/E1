2c6b2fe7a9ddcbc2f692d69c6d5d08eb78bc200a
AngeTarira1213 <Dayanita9082@hotmail.com>
2022-10-07 20:03:46 -0500

    examen

---
+++ compara.cpp
@@ -0,0 +1,30 @@
+//Creado por stalin franci
+// modificado ANGELICA ESTUPIÑAN 
+//Fecha:29 -09-2022
+#include<iostream>
+using namespace std;
+int main()
+
+{
+	float aea,aeb;
+	cout<<"ingrese a aeb:"; cin>>aea>>aeb;
+	if(aea>=aeb)
+	{
+	  cout<<aea<<" es mayor  "<<aeb<<endl;
+
+
+	}else{
+
+	  cout<<aeb<<" es mayor  "<<aea<<endl;
+
+
+	}
+
+	return 0;
+
+
+
+
+
+
+}
